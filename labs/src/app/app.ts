5a99f32c4aa73d70f44c17fe4ac97be20e725dac
Rafael Medeiros <rafaelfufcg@gmail.com>
2026-05-04 23:14:22 -0300

    feat: add header, footer, and main components with styles and tests
    - Created Footer component with HTML, CSS, and unit tests.
    - Created Header component with HTML, CSS, and unit tests.
    - Created Main component with HTML, CSS, and unit tests.

--- labs/src/app/app.ts
+++ labs/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Widget } from './widget/widget';
+import { Header } from "./header/header";
+import { Main } from "./main/main";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Widget],
+  imports: [Widget, Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
