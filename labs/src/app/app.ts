685165201f382a1198c1f107b205347bc698feda
Rafael Medeiros <rafaelfufcg@gmail.com>
2026-05-05 22:54:26 -0300

    refactor: remove Widget import and update footer structure

--- labs/src/app/app.ts
+++ labs/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { Widget } from './widget/widget';
 import { Header } from "./header/header";
 import { Main } from "./main/main";
 import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Widget, Header, Main, Footer],
+  imports: [Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
